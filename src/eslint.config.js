84ba0f2136ea54c8a24eb97ad9267e498a5a50c3
vansh211 <vanshmunjal111@gmail.com>
2026-04-23 11:56:16 +0530

    commit 5

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,16 +1,8 @@
-export default [
-  {
-    files: ["**/*.js"],
-    ignores: ["node_modules/**", "dist/**"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        console: "readonly",
-      },
-    },
-    rules: {
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    },
-  },
-];
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+]);
